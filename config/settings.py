663a57e74c62a5ba02c2a99ea6bc056fc92db273
Dhan-Rheb Belza <dhanrheb@gmail.com>
2021-02-27 18:18:42 +0800

    Add django-admin-interface

--- config/settings.py
+++ config/settings.py
@@ -35,6 +35,8 @@ ALLOWED_HOSTS = env("ALLOWED_HOSTS")
 # Application definition
 
 DJANGO_APPS = [
+    "admin_interface",
+    "colorfield",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
