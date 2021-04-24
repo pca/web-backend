6a5d5db9df822ad2aff5c7f4f2acb000a64edd42
Dhan-Rheb Belza <dhanrheb@gmail.com>
2021-04-24 09:16:57 +0800

    Fix clashing README.md location in data/ and wca data dump

--- scripts/import_wca_data.py
+++ scripts/import_wca_data.py
@@ -1,32 +1,37 @@
 import json
+import logging
 import math
 from pathlib import Path
 
-import pandas as pd
 import numpy as np
+import pandas as pd
+from django.conf import settings
 from django.db import transaction
 
 from wca.models import (
+    Championship,
+    Competition,
     Continent,
     Country,
     Event,
     Format,
-    RoundType,
-    Competition,
     Person,
     RanksAverage,
     RanksSingle,
     Result,
-    Championship,
+    RoundType,
 )
 
+log = logging.getLogger(__name__)
+
 PH_ID = "Philippines"
+DUMP_DIR = settings.BASE_DIR.joinpath("data/extracted/")
 
 
 @transaction.atomic
 def import_continents():
-    print("  importing continents")
-    df = pd.read_csv("data/WCA_export_Continents.tsv", sep="\t")
+    log.info("  importing continents")
+    df = pd.read_csv(DUMP_DIR.joinpath("WCA_export_Continents.tsv"), sep="\t")
     df = df.replace({np.nan: None})
 
     for row in df.itertuples():
@@ -44,8 +49,8 @@ def import_continents():
 
 @transaction.atomic
 def import_countries():
-    print("  importing countries")
-    df = pd.read_csv("data/WCA_export_Countries.tsv", sep="\t")
+    log.info("  importing countries")
+    df = pd.read_csv(DUMP_DIR.joinpath("WCA_export_Countries.tsv"), sep="\t")
     df = df.replace({np.nan: None})
 
     for row in df.itertuples():
@@ -61,8 +66,8 @@ def import_countries():
 
 @transaction.atomic
 def import_events():
-    print("  importing events")
-    df = pd.read_csv("data/WCA_export_Events.tsv", sep="\t")
+    log.info("  importing events")
+    df = pd.read_csv(DUMP_DIR.joinpath("WCA_export_Events.tsv"), sep="\t")
     df = df.replace({np.nan: None})
 
     for row in df.itertuples():
@@ -79,8 +84,8 @@ def import_events():
 
 @transaction.atomic
 def import_formats():
-    print("  importing formats")
-    df = pd.read_csv("data/WCA_export_Formats.tsv", sep="\t")
+    log.info("  importing formats")
+    df = pd.read_csv(DUMP_DIR.joinpath("WCA_export_Formats.tsv"), sep="\t")
     df = df.replace({np.nan: None})
 
     for row in df.itertuples():
@@ -99,8 +104,8 @@ def import_formats():
 
 @transaction.atomic
 def import_round_types():
-    print("  importing round types")
-    df = pd.read_csv("data/WCA_export_RoundTypes.tsv", sep="\t")
+    log.info("  importing round types")
+    df = pd.read_csv(DUMP_DIR.joinpath("WCA_export_RoundTypes.tsv"), sep="\t")
     df = df.replace({np.nan: None})
 
     for row in df.itertuples():
@@ -117,12 +122,12 @@ def import_round_types():
 
 @transaction.atomic
 def import_competitions():
-    df = pd.read_csv("data/WCA_export_Competitions.tsv", sep="\t")
+    df = pd.read_csv(DUMP_DIR.joinpath("WCA_export_Competitions.tsv"), sep="\t")
     df = df.replace({np.nan: None})
     total_chunks = math.ceil(len(df) / 1000)
 
     for index, group in df.groupby(np.arange(len(df)) // 1000):
-        print(f"  importing competitions chunk {index + 1}/{total_chunks}")
+        log.info(f"  importing competitions chunk {index + 1}/{total_chunks}")
         with transaction.atomic():
             for row in group.itertuples():
                 Competition.objects.update_or_create(
@@ -152,7 +157,7 @@ def import_competitions():
 
 
 def get_persons_df():
-    df = pd.read_csv("data/WCA_export_Persons.tsv", sep="\t")
+    df = pd.read_csv(DUMP_DIR.joinpath("WCA_export_Persons.tsv"), sep="\t")
     ph_df = df[df["countryId"] == PH_ID]
     ph_df = ph_df.replace({np.nan: None})
     return ph_df
@@ -160,7 +165,7 @@ def get_persons_df():
 
 @transaction.atomic
 def import_persons():
-    print("  importing persons")
+    log.info("  importing persons")
     df = get_persons_df()
 
     for row in df.itertuples():
@@ -177,10 +182,10 @@ def import_persons():
 
 @transaction.atomic
 def import_ranks_average():
-    print("  importing ranks average")
+    log.info("  importing ranks average")
     persons_df = get_persons_df()
 
-    df = pd.read_csv("data/WCA_export_RanksAverage.tsv", sep="\t")
+    df = pd.read_csv(DUMP_DIR.joinpath("WCA_export_RanksAverage.tsv"), sep="\t")
     ph_df = df[df["personId"].isin(persons_df["id"])]
     ph_df = ph_df.replace({np.nan: None})
 
@@ -199,10 +204,10 @@ def import_ranks_average():
 
 @transaction.atomic
 def import_ranks_single():
-    print("  importing ranks single")
+    log.info("  importing ranks single")
     persons_df = get_persons_df()
 
-    df = pd.read_csv("data/WCA_export_RanksSingle.tsv", sep="\t")
+    df = pd.read_csv(DUMP_DIR.joinpath("WCA_export_RanksSingle.tsv"), sep="\t")
     ph_df = df[df["personId"].isin(persons_df["id"])]
     ph_df = ph_df.replace({np.nan: None})
 
@@ -221,7 +226,7 @@ def import_ranks_single():
 
 @transaction.atomic
 def import_results():
-    df = pd.read_csv("data/WCA_export_Results.tsv", sep="\t")
+    df = pd.read_csv(DUMP_DIR.joinpath("WCA_export_Results.tsv"), sep="\t")
     ph_df = df[df["personCountryId"] == PH_ID]
     ph_df = ph_df.replace({np.nan: None})
     total_chunks = math.ceil(len(ph_df) / 1000)
@@ -229,7 +234,7 @@ def import_results():
     Result.objects.all().delete()
 
     for index, group in ph_df.groupby(np.arange(len(ph_df)) // 1000):
-        print(f"  importing results chunk {index + 1}/{total_chunks}")
+        log.info(f"  importing results chunk {index + 1}/{total_chunks}")
         with transaction.atomic():
             for row in group.itertuples():
                 Result.objects.create(
@@ -255,8 +260,8 @@ def import_results():
 
 @transaction.atomic
 def import_championships():
-    print("  importing championships")
-    df = pd.read_csv("data/WCA_export_championships.tsv", sep="\t")
+    log.info("  importing championships")
+    df = pd.read_csv(DUMP_DIR.joinpath("WCA_export_championships.tsv"), sep="\t")
     df = df.replace({np.nan: None})
 
     for row in df.itertuples():
@@ -281,16 +286,19 @@ def start_import():
     import_ranks_single()
     import_results()
     import_championships()
+    log.info("Data import successful!")
 
 
 def run():
-    if Path("data/previous_metadata.json").is_file():
-        with open("data/metadata.json") as metadata_file:
+    previous_metadata = DUMP_DIR.joinpath("previous_metadata.json")
+
+    if previous_metadata.is_file():
+        with open(DUMP_DIR.joinpath("metadata.json")) as metadata_file:
             metadata = json.load(metadata_file)
-        with open("data/previous_metadata.json") as previous_metadata_file:
+        with open(previous_metadata) as previous_metadata_file:
             previous_metadata = json.load(previous_metadata_file)
         if metadata["export_date"] == previous_metadata["export_date"]:
-            print("Data is up-to-date.")
+            log.info("Data is up-to-date.")
             return
 
     start_import()
--- sync_wca_database.sh
+++ sync_wca_database.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/env sh
 
 dir="$(dirname "$0")"
+data_dir="$dir/data"
+dump_dir="$data_dir/extracted"
 
 echo "Downloading WCA Database"
-curl https://www.worldcubeassociation.org/results/misc/WCA_export.tsv.zip -o "$dir/data/WCA_export.tsv.zip"
+curl https://www.worldcubeassociation.org/results/misc/WCA_export.tsv.zip -o "$data_dir/WCA_export.tsv.zip"
 
-if [ -e "$dir/data/metadata.json" ]
+if [ -e "$dump_dir/metadata.json" ]
 then
-    mv "$dir/data/metadata.json" "$dir/data/previous_metadata.json"
+    mv "$dump_dir/metadata.json" "$dump_dir/previous_metadata.json"
 fi
 
 echo "Unzipping archive"
-unzip -o "$dir/data/WCA_export.tsv.zip" -d "$dir/data/"
+unzip -o "$data_dir/WCA_export.tsv.zip" -d "$dump_dir"
 
 echo "Importing WCA data"
 python "$dir/manage.py" runscript import_wca_data
